7dc1640287a490ad7f388bd7b0e599d3b88efcb3
Akinori Ito <voice0726@gmail.com>
2024-05-22 18:53:33 +0900

    refactor: modify styling, and remove comments and unused function

--- app/auth/auth.server.ts
+++ app/auth/auth.server.ts
@@ -1,6 +1,6 @@
 import { Authenticator } from 'remix-auth';
 import { sessionStorage } from '~/auth/session.server';
-import { OAuth2Profile, OAuth2Strategy } from 'remix-auth-oauth2';
+import { OAuth2Strategy } from 'remix-auth-oauth2';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 
 export type Session = {
@@ -14,16 +14,7 @@ export type User = {
   email: string;
 };
 
-// Create an instance of the authenticator, pass a generic with what
-// strategies will return and will store in the session
 export const authenticator = new Authenticator<Session>(sessionStorage);
-
-export async function getUser(profile: OAuth2Profile): Promise<User> {
-  const email = profile.emails?.[0]?.value;
-  const id = profile.id ?? '';
-
-  return { id, email: email ?? '' };
-}
 
 const getUserFromToken = (idToken: string) => {
   const { traits, sub } = jwt.decode(idToken) as JwtPayload & { traits: { email: string } };
@@ -43,17 +34,12 @@ export const oauth2Strategy = new OAuth2Strategy(
     authenticateWith: 'request_body',
   },
   async ({ tokens }) => {
-    // here you can use the params above to get the user and return it
-    // what you do inside this and how you find the user is up to you
-    const user = getUserFromToken(tokens.id_token);
-
-    return { accessToken: tokens.access_token, refreshToken: tokens.refresh_token, user };
+    return {
+      accessToken: tokens.access_token,
+      refreshToken: tokens.refresh_token,
+      user: getUserFromToken(tokens.id_token),
+    };
   },
 );
 
-authenticator.use(
-  oauth2Strategy,
-  // this is optional, but if you setup more than one OAuth2 instance you will
-  // need to set a custom name to each one
-  'ore-no-idp',
-);
+authenticator.use(oauth2Strategy, 'ore-no-idp');
