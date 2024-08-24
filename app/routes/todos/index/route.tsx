905468b253ce509c76ff6ba849b3b60b4bdafbef
Akinori Ito <voice0726@gmail.com>
2024-08-24 16:11:34 +0900

    feat: add react-table

--- app/routes/todos/index/route.tsx
+++ app/routes/todos/index/route.tsx
@@ -1,6 +1,8 @@
 import { json, LoaderFunctionArgs } from '@remix-run/node';
 import { checkSession } from '~/auth/auth.server';
 import { useLoaderData } from '@remix-run/react';
+import { Todo } from '~/types';
+import { createColumnHelper, flexRender, getCoreRowModel, useReactTable } from '@tanstack/react-table';
 
 export async function loader({ request }: LoaderFunctionArgs) {
   const { session, headers } = await checkSession(request);
@@ -8,7 +10,7 @@ export async function loader({ request }: LoaderFunctionArgs) {
     headers: { authorization: `Bearer: ${session.accessToken}` },
   }).then(async (res) => {
     if (res.status === 200) {
-      return await res.json();
+      return (await res.json()) as Todo[];
     }
     throw new Error('Failed to fetch todos');
   });
@@ -16,17 +18,54 @@ export async function loader({ request }: LoaderFunctionArgs) {
   return json({ todos: res }, { headers });
 }
 
+const columnHelper = createColumnHelper<Todo>();
+const columns = [
+  columnHelper.accessor('id', {
+    cell: (info) => info.getValue(),
+  }),
+  columnHelper.accessor('title', {
+    cell: (info) => info.getValue(),
+  }),
+  columnHelper.accessor('description', {
+    cell: (info) => info.getValue(),
+  }),
+];
+
 export default function DashboardRoute() {
   const { todos } = useLoaderData<typeof loader>();
+  const table = useReactTable<Todo>({
+    columns,
+    data: todos,
+    getCoreRowModel: getCoreRowModel(),
+  });
 
   return (
     <div className="text-sm">
-      <h1 className="text-2xl">Todos</h1>
-      {todos.map((todo: any) => (
-        <div key={todo.id}>
-          {todo.id}: {todo.title} - {todo.description}
-        </div>
-      ))}
+      <h1>React table</h1>
+      <table>
+        <thead>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id} className="border bg-gray-400">
+              {headerGroup.headers.map((header) => (
+                <th key={header.id} className="border">
+                  {flexRender(header.column.columnDef.header, header.getContext())}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map((row) => (
+            <tr key={row.id} className="border">
+              {row.getVisibleCells().map((cell) => (
+                <td key={cell.id} className="border">
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
