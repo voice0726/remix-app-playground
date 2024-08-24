8d819a51a6ebed9c4cd33ceb3e06935ccc479d21
Akinori Ito <voice0726@gmail.com>
2024-08-24 16:08:24 +0900

    feat: change oauth2 server to zitadel

--- app/auth/auth.server.ts
+++ app/auth/auth.server.ts
@@ -6,6 +6,7 @@ import jwt, { JwtPayload } from 'jsonwebtoken';
 export type Session = {
   accessToken: string;
   refreshToken?: string;
+  idToken?: string;
   user?: User;
 };
 
@@ -17,26 +18,27 @@ export type User = {
 export const authenticator = new Authenticator<Session>(sessionStorage, { sessionKey: 'session' });
 
 const getUserFromToken = (idToken: string) => {
-  const { traits, sub } = jwt.decode(idToken) as JwtPayload & { traits: { email: string } };
-  return { id: sub ?? '', email: traits.email };
+  const { email, sub } = jwt.decode(idToken) as JwtPayload & { email: string };
+
+  return { id: sub ?? '', email };
 };
 
 export const oauth2Strategy = new OAuth2Strategy(
   {
-    authorizationEndpoint: 'http://localhost:8444/oauth2/auth',
-    tokenEndpoint: 'http://localhost:8444/oauth2/token',
-    tokenRevocationEndpoint: 'http://localhost:8444/oauth2/revoke',
-    clientId: 'remix-app',
-    clientSecret: 'secret',
-    redirectURI: 'http://localhost:3000/auth/callback',
-    scopes: ['openid', 'offline_access'],
+    authorizationEndpoint: process.env.AUTHORIZATION_ENDPOINT_URL ?? '',
+    tokenEndpoint: process.env.TOKEN_ENDPOINT_URL ?? '',
+    tokenRevocationEndpoint: process.env.TOKEN_REVOCATION_ENDPOINT_URL ?? '',
+    clientId: '281641775037218818',
+    clientSecret: '',
+    redirectURI: process.env.AUTH_CALLBACK_URL ?? '',
+    scopes: ['openid', 'email', 'offline_access'],
     codeChallengeMethod: 'S256',
     authenticateWith: 'request_body',
   },
   async ({ tokens }) => {
-    console.log(tokens);
     return {
       accessToken: tokens.access_token,
+      idToken: tokens.id_token,
       refreshToken: tokens.refresh_token,
       user: getUserFromToken(tokens.id_token),
     };
@@ -50,29 +52,29 @@ export const checkSession = async (request: Request): Promise<{ session: Session
     failureRedirect: '/login',
   });
 
-  const res = await fetch(`http://localhost:8444/userinfo`, {
+  const res = await fetch(process.env.USERINFO_ENDPOINT_URL ?? '', {
     headers: { authorization: `Bearer ${session.accessToken}` },
   });
   if (res.status === 401) {
     if (!session.refreshToken) {
       console.log('no refresh token, redirect to login');
       return await authenticator.logout(request, { redirectTo: '/login' });
-    } else {
-      const { access_token, refresh_token } = await oauth2Strategy.refreshToken(session.refreshToken);
-      console.log('current tokens', session.accessToken, session.refreshToken);
-      console.log('refreshed tokens', access_token, refresh_token);
-
-      const s = await getSession(request.headers.get('Cookie'));
-      s.set(authenticator.sessionKey, {
-        ...s.get(authenticator.sessionKey),
-        accessToken: access_token,
-        refreshToken: refresh_token,
-      });
-      session.refreshToken = refresh_token;
-      session.accessToken = access_token;
-
-      return { session, headers: { 'Set-Cookie': await commitSession(s) } };
     }
+
+    const { access_token, refresh_token } = await oauth2Strategy.refreshToken(session.refreshToken);
+    console.log('current tokens', session.accessToken, session.refreshToken);
+    console.log('refreshed tokens', access_token, refresh_token);
+
+    const s = await getSession(request.headers.get('Cookie'));
+    s.set(authenticator.sessionKey, {
+      ...s.get(authenticator.sessionKey),
+      accessToken: access_token,
+      refreshToken: refresh_token,
+    });
+    session.refreshToken = refresh_token;
+    session.accessToken = access_token;
+
+    return { session, headers: { 'Set-Cookie': await commitSession(s) } };
   }
   return { session };
 };
--- app/routes/logout.tsx
+++ app/routes/logout.tsx
@@ -1,11 +1,38 @@
-import { ActionFunctionArgs } from '@remix-run/node';
-import { authenticator, oauth2Strategy } from '~/auth/auth.server';
+import { createCookie, LoaderFunctionArgs, redirect } from '@remix-run/node';
+import { authenticator } from '~/auth/auth.server';
+import Crypto from 'crypto';
 
-export async function loader({ request }: ActionFunctionArgs) {
+const logoutStateCookie = createCookie('logout_state', {
+  path: '/',
+  httpOnly: true,
+  maxAge: 60 * 15,
+  secure: process.env.NODE_ENV === 'production',
+  sameSite: 'lax',
+});
+
+export async function loader({ request }: LoaderFunctionArgs) {
   const user = await authenticator.isAuthenticated(request, {
     failureRedirect: '/login',
   });
 
-  await oauth2Strategy.revokeToken(user.accessToken);
+  const url = new URL(request.url);
+  const state = url.searchParams.get('state');
+  if (!state) {
+    const state = Crypto.randomBytes(20).toString('hex');
+    return redirect(
+      `http://localhost:8080/oidc/v1/end_session?id_token_hint=${user.idToken}&post_logout_redirect_uri=http://localhost:3000/logout&state=${state}`,
+      {
+        headers: {
+          'Set-Cookie': await logoutStateCookie.serialize({ logoutState: state }),
+        },
+      },
+    );
+  }
+
+  const cookie = await logoutStateCookie.parse(request.headers.get('Cookie')) as { logoutState: string };
+  if (state !== cookie.logoutState) {
+    throw new Error('Invalid state');
+  }
+
   return await authenticator.logout(request, { redirectTo: '/login' });
 }
